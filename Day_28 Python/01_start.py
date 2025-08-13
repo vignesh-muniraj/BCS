74fb109ae4769bbd2c86691049fe64eb27bc77f9
vignesh-muniraj <mnvicky04@gmail.com>
2025-08-13 18:15:25 +0530

    without  stageing

---
+++ Day_28 Python/01_start.py
@@ -0,0 +1,7 @@
+print("helo")
+
+salary = 50_000    # numeric separator
+print(salary)
+print(type(salary))   #int                                       
+
+# print(7//2)
---
+++ Day_28 Python/02_input.py
@@ -0,0 +1,37 @@
+# name = input("Tell me your name?: ")
+# dob = int(input("Tell me your birth?: "))
+# age = 2025 - dob
+# print("Hi, " + name + " you are " + str(age) +" years old. " )
+# print(f"Hi, {name} you are {2025 - dob} years old")
+
+# Fahrenheit = float(input("Please Provide your Fahrenheit: "))
+# celsius = float((Fahrenheit- 32) * 5/9)
+# print(f"The {Fahrenheit}°F is {celsius}°C") 
+
+# ---------------------------
+# radius = float(input("Provide the radius of the circle: "))
+# area = radius**2 * 3.14
+# print(f"Area of the Circle is: {area}")
+
+#-----------------------------------
+
+# value = int(input("Enter percentage ")) #23
+# res = int(value // 10)   # 2
+# equal = "="*res 
+# space = " "*(10-res)
+# # print(f"[{ "="*res, '  '*(10-int(res))}] {value}%")
+# print(f"[{equal}{space}] {value}%")
+
+#------------------------------------------
+
+fav = "John Wick"
+print(fav[5:9])
+print(fav[-4:])
+print(fav[-4:-1])
+print(fav[:])  # copy string
+print(fav[5:9:2])
+print(fav[::-1]) # reverse
+
+
+
+
