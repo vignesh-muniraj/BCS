ac61f4c8d96c33c2db8143740387e72376d276ee
vignesh-muniraj <mnvicky04@gmail.com>
2025-08-14 15:10:31 +0530

    operators

--- Day_28 Python/03_operators.py
+++ Day_28 Python/03_operators.py
@@ -1,2 +1,21 @@
-print("helo")
+fav_movie = "John Wick"
+
+print(fav_movie[0]) # J
+print(len(fav_movie)) # 9
+
+print(fav_movie[8]) # k
+
+## -ve index
+print(fav_movie[-1]) # k
+
+# Task 4.1: (+ve)
+# Wick 
+
+print(fav_movie[5] + fav_movie[6] + fav_movie[7]+ fav_movie[8])
+
+# Task 4.2: (-ve)
+# Wick 
+
+print(fav_movie[-4] + fav_movie[-3] + fav_movie[-2]+ fav_movie[-1])
+
 
